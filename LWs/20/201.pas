23c24930955de0a01c3b21777ada0cb7436f6907
Ivan <61554880+johnforestbear@users.noreply.github.com>
2020-05-13 22:42:41 +0300

    Update 201.pas

--- LWs/20/201.pas
+++ LWs/20/201.pas
@@ -8,7 +8,7 @@ TYPE
   Sets = SET OF MinValueOfCoord .. MaxValueOfCoord;
 VAR
   Symbol: CHAR;
-PROCEDURE PutCoordinatesToSet(VAR Symbol: CHAR);
+PROCEDURE PrintSymbolByCoord(VAR Symbol: CHAR);
   VAR
     Indication: INTEGER;
     ProgramError: BOOLEAN;
@@ -22,19 +22,19 @@ PROCEDURE PutCoordinatesToSet(VAR Symbol: CHAR);
           'B': SetForTyping := [1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 16, 20, 21, 22, 23, 24];
           'C': SetForTyping := [2, 3, 4, 5, 6, 11, 16, 22, 23, 24, 25];
           'D': SetForTyping := [1, 2, 3, 4, 6, 10, 11, 15, 16, 20, 21, 22, 23, 24];
-          'E': SetForTyping := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25];
-          'F': SetForTyping := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25];
-          'G': SetForTyping := [1, 2, 3, 4, 5, 6, 11, 14, 15, 16, 20, 21, 22, 23, 24, 25];
+          'E': SetForTyping := [2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 22, 23, 24, 25];
+          'F': SetForTyping := [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21];
+          'G': SetForTyping := [2, 3, 4, 5, 6, 11, 14, 15, 16, 20, 22, 23, 24, 25];
           'H': SetForTyping := [1, 5, 6, 10, 11, 12, 13, 14, 15, 16, 20, 21, 25];
           'I': SetForTyping := [2, 3, 4, 8, 13, 18, 22, 23, 24];
-          'J': SetForTyping := [1, 2, 3, 4, 5, 8, 13, 16, 18, 21, 22];
+          'J': SetForTyping := [1, 2, 3, 4, 5, 8, 13, 18, 21, 22];
           'K': SetForTyping := [1, 4, 6, 8, 11, 12, 16, 18, 21, 24];
           'L': SetForTyping := [1, 6, 11, 16, 21, 22, 23, 24, 25];
           'M': SetForTyping := [1, 5, 6, 7, 9, 10, 11, 13, 15, 16, 20, 21, 25];
           'N': SetForTyping := [1, 5, 6, 7, 10, 11, 13, 15, 16, 19, 20, 21, 25];
           'O': SetForTyping := [2, 3, 4, 6, 10, 11, 15, 16, 20, 22, 23, 24];
-          'P': SetForTyping := [1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 15, 16, 21];
-          'Q': SetForTyping := [1, 2, 3, 4, 6, 9, 11, 14, 16, 17, 18, 19, 25];
+          'P': SetForTyping := [1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 16, 21];
+          'Q': SetForTyping := [2, 3, 6, 9, 11, 14, 17, 18, 19, 25];
           'R': SetForTyping := [1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 16, 19, 21, 25];
           'S': SetForTyping := [2, 3, 4, 5, 6, 12, 13, 14, 20, 21, 22, 23, 24];
           'T': SetForTyping := [1, 2, 3, 4, 5, 8, 13, 18, 23];
@@ -72,5 +72,5 @@ BEGIN
   IF NOT EOLN
   THEN
     READ(INPUT, Symbol);
-  PutCoordinatesToSet(Symbol)
+  PrintSymbolByCoord(Symbol)
 END.
