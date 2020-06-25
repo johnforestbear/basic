e11b766993dffd18c3078cbb4a93d15d8379c397
Ivan <61554880+johnforestbear@users.noreply.github.com>
2020-06-25 22:14:16 +0300

    241 add picture

--- LWs/24/241.pas
+++ LWs/24/241.pas
@@ -1,90 +1,50 @@
-PROGRAM Encryption(INPUT, OUTPUT);
-{Переводит символы из INPUT в код согласно Chiper 
-  и печатает новые символы в OUTPUT}
-CONST
-  Len = 20;
-  FirstSymOfCpr = ' ';
-  LastSymOfCpr = 'Z';
-TYPE
-  Str = ARRAY [1 .. Len] OF CHAR;
-  Chiper = ARRAY [FirstSymOfCpr .. LastSymOfCpr] OF CHAR;
+п»їPROGRAM TreeSort(INPUT, OUTPUT);
+TYPE 
+  Tree = ^NodeType;
+  NodeType = RECORD
+               Key: CHAR;
+               LLink, RLink: Tree
+             END;
 VAR
-  Msg: Str;   
-  Code: Chiper;
-  LenOfStr: INTEGER;
-
-PROCEDURE Initialize(VAR Code: Chiper);
-{Присвоить Code шифр замены}
-BEGIN {Initialize}
-  Code[' '] := '@';
-  Code['A'] := 'Z';
-  Code['B'] := 'Y';
-  Code['C'] := 'X';
-  Code['D'] := 'W';
-  Code['E'] := 'V';
-  Code['F'] := 'U';
-  Code['G'] := 'T';
-  Code['H'] := 'S';
-  Code['I'] := 'R';
-  Code['J'] := 'Q';
-  Code['K'] := 'P';
-  Code['L'] := 'O';
-  Code['M'] := 'N';
-  Code['N'] := 'M';
-  Code['O'] := 'L';
-  Code['P'] := 'K';
-  Code['Q'] := 'J';
-  Code['R'] := 'I';
-  Code['S'] := 'H';
-  Code['T'] := 'G';
-  Code['U'] := 'F';
-  Code['V'] := 'E';
-  Code['W'] := 'D';
-  Code['X'] := 'C';
-  Code['Y'] := 'B';
-  Code['Z'] := 'A'
-END;  {Initialize}
-
-PROCEDURE Encode(VAR S: Str; VAR Code: Chiper);
-{Выводит символы из Code, соответствующие символам из S}
-VAR
-  Index: 1 .. Len;
-BEGIN {Encode}
-  IF LenOfStr = 0
+  Root: Tree;
+  Ch: CHAR;
+  
+PROCEDURE Insert(VAR Ptr: Tree; Data: CHAR);
+BEGIN {Insert}
+  IF Ptr = NIL
   THEN
-    WRITE('empty line above')
+    BEGIN {РЎРѕР·РґР°РµРј Р»РёСЃС‚ СЃРѕ Р·РЅР°С‡РµРЅРёРµРј Data}
+      NEW(Ptr);
+      Ptr^.Key := DATA;
+      Ptr^.LLink := NIL;
+      Ptr^.RLink := NIL
+    END
   ELSE
-    BEGIN
-      FOR Index := 1 TO LenOfStr
-      DO
-        IF S[Index] IN [' ', 'A' .. 'Z']
-        THEN
-          WRITE(Code[S[Index]])
-        ELSE
-          WRITE(S[Index])
-    END;
-  WRITELN
-END;  {Encode}
+    IF Data <= Ptr^.Key
+    THEN
+      Insert(Ptr^.LLink, Data)
+    ELSE
+      Insert(Ptr^.RLink, Data)
+END;  {Insert}
 
-BEGIN {Encryption}
-  {Инициализировать Code}
-  Initialize(Code);
-  WHILE NOT EOF
+PROCEDURE PrintTree(Ptr: Tree);
+BEGIN {PrintTree}
+  IF Ptr <> NIL
+  THEN  {РџРµС‡Р°С‚Р°РµС‚ РїРѕРґРґРµСЂРµРІРѕ СЃР»РµРІР°, РІРµСЂС€РёРЅСѓ, РїРѕРґРґРµСЂРµРІРѕ СЃРїСЂР°РІР°}
+    BEGIN
+      PrintTree(Ptr^.LLink);
+      WRITE(Ptr^.Key);
+      PrintTree(Ptr^.RLink)
+    END
+END;  {PrintTree}
+
+BEGIN {TreeSort}
+  Root := NIL;
+  WHILE NOT EOLN
   DO
     BEGIN
-      {читать строку в Msg и распечатать ее}
-      LenOfStr := 0;
-      WHILE NOT EOLN AND (LenOfStr < Len)
-      DO
-        BEGIN
-          LenOfStr := LenOfStr + 1;
-          READ(Msg[LenOfStr]);
-          {эхо ввода}
-          WRITE(Msg[LenOfStr])
-        END;
-      READLN;
-      WRITELN;
-      {распечатать кодированное сообщение}
-      Encode(Msg, Code)
-    END
-END.  {Encryption}
+      READ(Ch);
+      Insert(Root, Ch)
+    END;
+  PrintTree(Root)
+END.  {TreeSort}
